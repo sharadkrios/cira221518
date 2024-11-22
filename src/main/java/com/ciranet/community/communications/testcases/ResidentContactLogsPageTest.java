153a51c9b1e5dd05a852b91a224808ce8fa67f65
sharadkrios <111059670+sharadkrios@users.noreply.github.com>
2024-11-22 16:11:04 +0530

    Update ResidentContactLogsPageTest.java

--- src/main/java/com/ciranet/community/communications/testcases/ResidentContactLogsPageTest.java
+++ src/main/java/com/ciranet/community/communications/testcases/ResidentContactLogsPageTest.java
@@ -26,7 +26,8 @@ public class ResidentContactLogsPageTest extends TestBase
 		residentContactLogs = new ResidentContactLogsPage(driver);
 
 		String communitySearchKeyword = "Amberwood";
-		assertTrue(residentContactLogs.verifySearchCommunity(communitySearchKeyword), "Resident Contact Log is not seen");
+		//assertTrue(residentContactLogs.verifySearchCommunity(communitySearchKeyword), "Resident Contact Log is not seen");
+        residentContactLogs.verifySearchCommunity(communitySearchKeyword);
 	}
 	
 	@Test(priority = 1, description = "Verify Resident Contact Logs Historical hyperlinks", groups = {Constants.FUNCTIONAL_TESTING}, alwaysRun = true)
@@ -35,6 +36,7 @@ public class ResidentContactLogsPageTest extends TestBase
 		LoggerManager.info("Verifying Resident Contact Logs Historical hyperlinks");
 		TestBase.setExtentReportSettings("Resident Contact Logs Historical hyperlinks", Constants.FUNCTIONAL_TESTING, "Resident Contact Logs", "Verifying Resident Contact Logs Historical hyperlinks");
 		residentContactLogs = new ResidentContactLogsPage(driver);
-		assertTrue(residentContactLogs.verifyHistoricalHyperlink(), "Resident Contact Log Detail Historical hyperlinks are not seen");
+		//assertTrue(residentContactLogs.verifyHistoricalHyperlink(), "Resident Contact Log Detail Historical hyperlinks are not seen");
+        residentContactLogs.verifyHistoricalHyperlink();
 	}
 }
