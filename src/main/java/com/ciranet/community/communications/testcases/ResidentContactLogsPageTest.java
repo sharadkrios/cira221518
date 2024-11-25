faf2bb9f34bd7e94e371550d0f419d2d42412609
sharadkrios <111059670+sharadkrios@users.noreply.github.com>
2024-11-25 09:42:10 +0530

    Update ResidentContactLogsPageTest.java

--- src/main/java/com/ciranet/community/communications/testcases/ResidentContactLogsPageTest.java
+++ src/main/java/com/ciranet/community/communications/testcases/ResidentContactLogsPageTest.java
@@ -21,22 +21,19 @@ public class ResidentContactLogsPageTest extends TestBase
 
 	public void verifyHomeOwnerSearch() 
 	{
-		LoggerManager.info("Verifying residentContactLogs - Community Search");
+		String communitySearchKeyword = "Amberwood";
+		LoggerManager.info("Verify Search Community result: " + residentContactLogs.verifySearchCommunity(communitySearchKeyword));
 		TestBase.setExtentReportSettings("Resident Contact Logs", Constants.SMOKE_TESTING, "Verify Home Page Community Search", "Verifying Community Search");
 		residentContactLogs = new ResidentContactLogsPage(driver);
-
-		String communitySearchKeyword = "Amberwood";
-		assertTrue(residentContactLogs.verifySearchCommunity(communitySearchKeyword), "Resident Contact Log is not seen");
-        
+		assertTrue(residentContactLogs.verifySearchCommunity(communitySearchKeyword), "Resident Contact Log Logs is not seen");
 	}
 	
 	@Test(priority = 1, description = "Verify Resident Contact Logs Historical hyperlinks", groups = {Constants.FUNCTIONAL_TESTING}, alwaysRun = true)
 	public void verfiyHistoricalHyperlink() throws TimeoutException 
 	{
-		LoggerManager.info("Verifying Resident Contact Logs Historical hyperlinks");
+		LoggerManager.info("Verify Historical Hyperlink result: " + residentContactLogs.verifyHistoricalHyperlink());
 		TestBase.setExtentReportSettings("Resident Contact Logs Historical hyperlinks", Constants.FUNCTIONAL_TESTING, "Resident Contact Logs", "Verifying Resident Contact Logs Historical hyperlinks");
 		residentContactLogs = new ResidentContactLogsPage(driver);
 		assertTrue(residentContactLogs.verifyHistoricalHyperlink(), "Resident Contact Log Detail Historical hyperlinks are not seen");
-        
 	}
 }
