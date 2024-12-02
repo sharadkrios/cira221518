4676d8f0dbdc82836116bef735a3098ad51ab562
sharadkrios <111059670+sharadkrios@users.noreply.github.com>
2024-12-02 11:30:15 +0530

    Update ResidentContactLogsPageTest.java

--- src/main/java/com/ciranet/community/communications/testcases/ResidentContactLogsPageTest.java
+++ src/main/java/com/ciranet/community/communications/testcases/ResidentContactLogsPageTest.java
@@ -26,8 +26,7 @@ public class ResidentContactLogsPageTest extends TestBase {
         String communitySearchKeyword = "Amberwood"; // Define the search keyword.
 
         // Validate navigation to the page.
-        assertTrue(residentContactLogs.navigate(communitySearchKeyword),
-                "Error navigating to Resident Contact Logs page.");
+        residentContactLogs.navigate(communitySearchKeyword);
     }
 
     @Test(priority = 1, description = "Verify Resident Contact Logs Historical Hyperlink", groups = {Constants.FUNCTIONAL_TESTING}, alwaysRun = true)
@@ -38,8 +37,7 @@ public class ResidentContactLogsPageTest extends TestBase {
         residentContactLogs = new ResidentContactLogsPage(driver);
 
         // Validate historical hyperlink functionality.
-        assertTrue(residentContactLogs.verifyResidentContactLogsHistoricalHyperlink(),
-                "Resident Contact Log Detail Historical hyperlink is not functional or not visible.");
+        residentContactLogs.verifyResidentContactLogsHistoricalHyperlink();
         LoggerManager.info("Test passed: Historical hyperlinks in Resident Contact Logs verified successfully.");
     }
 }
